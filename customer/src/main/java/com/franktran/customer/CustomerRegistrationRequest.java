42a7a4dd10dfc3d7630232fbdacae0c501545f77
Tu Tran <ttran.larion@absolute.com>
2022-05-26 23:48:13 +0700

    Model Controller and Service

---
+++ customer/src/main/java/com/franktran/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.franktran.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+}
---
+++ customer/src/main/java/com/franktran/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.franktran.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest customerRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRequest.firstName())
+                .lastName(customerRequest.lastName())
+                .email(customerRequest.email())
+                .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        // todo: store customer in db
+    }
+}
